8bd972c540f2f8e886db2cc22dcd1829c28fe873
Joshua Wee <joshua@me3.io>
2023-03-01 23:21:36 +0800

    chore: fixing connectWallet

--- src/components /Trade/TradeInput.tsx
+++ src/components /Trade/TradeInput.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import SendIcon from '@mui/icons-material/Send';
+import { useWallet } from "@solana/wallet-adapter-react"
+import { useWalletModal, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 
 const TradeInput = () => {
+  const {connected, disconnect, publicKey} = useWallet()
+  const { setVisible } = useWalletModal()
+
+  const connectWallet = () => {
+    setVisible(true);
+  };
   return (
-    <div className='bg-[#2d42fc] h-1/5 items-center flex justify-between '>
-        <div></div>
-        <input type="text" placeholder='Enter an amount' className='bg-[#2d42fc] text-center'/>
-        <SendIcon className='pr-2 cursor-pointer'/>
+    <div className='bg-[#2d42fc] h-1/5 items-center flex flex-row justify-between cursor-pointer'>
+      {!connected ? ( 
+         <button>
+            <AccountBalanceWalletIcon/>
+           <WalletMultiButton/>
+         </button>
+      ): (
+        <>
+          <div></div>
+          <input type="text" placeholder='Enter an amount' className='bg-[#2d42fc] text-center'/>
+          <SendIcon className='pr-2 cursor-pointer'/>
+        </>
+        )
+      }
+       
     </div>
   )
 }
